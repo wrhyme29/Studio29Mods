d1cdb2b165a8e6ce26aea2c2d68c86064737e1bf
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-09-24 11:10:31 -0400

    new implementation

--- Controller/Environments/TheDeliOfDisaster/Cards/BitOfAPickleCardController.cs
+++ Controller/Environments/TheDeliOfDisaster/Cards/BitOfAPickleCardController.cs
@@ -1,6 +1,6 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -24,8 +24,10 @@ namespace Studio29.TheDeliOfDisaster
             //Listed win or loss conditions on cards are not in effect and may not be used to win or lose the game.
             AddTrigger((GameOverAction action) => action.EndingResult != EndingResult.HeroesDestroyedDefeat && action.EndingResult != EndingResult.VillainDestroyedVictory && action.EndingResult != EndingResult.EnvironmentDefeat, action => CancelActionProxy(action), TriggerType.CancelAction, TriggerTiming.Before);
 
-            //At the start of the environment turn, one player may discard their hand to shuffle this card back into the environment deck, ignoring its indestructability.
-            AddStartOfTurnTrigger(tt => tt == TurnTaker, ShuffleBackIntoDeckResponse, TriggerType.ShuffleCardIntoDeck);
+            //If a player ever has no cards in hand, shuffle this card back into the environment deck, ignoring its indestructability.
+            AddTrigger((MoveCardAction mca) => mca.Origin.IsHand && mca.Origin.NumberOfCards == 0 && mca.Origin != mca.Destination, ShuffleBackIntoDeckResponse, TriggerType.ShuffleCardIntoDeck, TriggerTiming.After);
+            AddTrigger((PlayCardAction pca) => pca.Origin.IsHand && pca.Origin.NumberOfCards == 0, ShuffleBackIntoDeckResponse, TriggerType.ShuffleCardIntoDeck, TriggerTiming.After);
+            AddTrigger((BulkMoveCardsAction bmca) => bmca.Origins.Any(origin => origin.Value.IsHand && origin.Value.NumberOfCards == 0 && origin.Value != bmca.Destination), ShuffleBackIntoDeckResponse, TriggerType.ShuffleCardIntoDeck, TriggerTiming.After);
         }
 
         private IEnumerator CancelActionProxy(GameOverAction action)
@@ -50,23 +52,10 @@ namespace Studio29.TheDeliOfDisaster
             }
         }
 
-        private IEnumerator ShuffleBackIntoDeckResponse(PhaseChangeAction pca)
+        private IEnumerator ShuffleBackIntoDeckResponse(GameAction action)
         {
-            List<SelectTurnTakerDecision> storedResultsHero = new List<SelectTurnTakerDecision>();
-            IEnumerator coroutine = GameController.SelectHeroToDiscardTheirHand(DecisionMaker, optionalSelectHero: true, optionalDiscardCards: false, storedResultsTurnTaker: storedResultsHero, cardSource: GetCardSource());
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(coroutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(coroutine);
-            }
-            if (!DidDiscardHand(storedResultsHero))
-            {
-                yield break;
-            }
-            coroutine = GameController.MoveCard(TurnTakerController, Card, TurnTaker.Deck, evenIfIndestructible: true, cardSource: GetCardSource());
+            
+            IEnumerator coroutine = GameController.MoveCard(TurnTakerController, Card, TurnTaker.Deck, evenIfIndestructible: true, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
