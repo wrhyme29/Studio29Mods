7abecfec13df7630d5218ed2213fecb02c6afbcf
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-19 23:18:00 -0400

    New Lions R1

---
+++ Controller/Heroes/TheTamer/Cards/DariusTheBoastfulCardController.cs
@@ -0,0 +1,42 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Studio29.TheTamer
+{
+    public class DariusTheBoastfulCardController : LionCardController
+    {
+
+        public DariusTheBoastfulCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, TriggerType.DealDamage)
+        {
+
+        }
+
+        protected override IEnumerator DealtExactlyOneDamageResponse(DealDamageAction dd)
+        {
+            //it deals each non-hero target 2 melee damage and 1 other hero target 2 melee damage.
+            IEnumerator coroutine = DealDamage(Card, c => !c.IsHero && c.IsTarget, 2, DamageType.Melee);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, Card), 2, DamageType.Melee, 1, false, 1, additionalCriteria: (Card c) => c.IsHero && c.IsTarget && c != Card, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+
+    }
+}
---
+++ Controller/Heroes/TheTamer/Cards/MistyTheYoungCardController.cs
@@ -0,0 +1,25 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Studio29.TheTamer
+{
+    public class MistyTheYoungCardController : LionCardController
+    {
+
+        public MistyTheYoungCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, TriggerType.DealDamage)
+        {
+
+        }
+
+        protected override IEnumerator DealtExactlyOneDamageResponse(DealDamageAction dd)
+        {
+            //it deals 2 other targets 1 melee damage each.
+            return GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, Card), 1, DamageType.Melee, 2, false, 2, additionalCriteria: (Card c) => c != Card, cardSource: GetCardSource());
+        }
+
+
+    }
+}
