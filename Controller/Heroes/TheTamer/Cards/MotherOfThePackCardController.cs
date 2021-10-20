84fbb3e5c9429ce806d4d88f1ff63c378e7f946e
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-20 01:04:25 -0400

    Fixed Mother of the Pack implementation

--- Controller/Heroes/TheTamer/Cards/MotherOfThePackCardController.cs
+++ Controller/Heroes/TheTamer/Cards/MotherOfThePackCardController.cs
@@ -13,15 +13,36 @@ namespace Studio29.TheTamer
 
         }
 
+        private static readonly string MarkForRetaliationKey = "MarkForRetaliation";
+
         public override void AddTriggers()
         {
             //You may redirect any damage dealt to other lion cards to this card. If this card was dealt damage this way, this card deals the source of that damage 3 melee damage.
-            AddTrigger((DealDamageAction dd) => IsLion(dd.Target) && dd.Target != Card && (dd.BattleZone == null || dd.BattleZone == Card.BattleZone), RedirectDamageAndRetaliateResponse, new TriggerType[] { TriggerType.RedirectDamage, TriggerType.DealDamage }, TriggerTiming.Before);
+            AddTrigger((DealDamageAction dd) => IsLion(dd.Target) && dd.Target != Card && (dd.BattleZone == null || dd.BattleZone == Card.BattleZone), RedirectDamageResponse, new TriggerType[] { TriggerType.RedirectDamage, TriggerType.DealDamage }, TriggerTiming.Before);
+            AddTrigger((DealDamageAction dd) => dd.DidDealDamage && dd.Target == Card && dd.DamageSource != null && dd.DamageSource.IsCard && GetCardPropertyJournalEntryBoolean(MarkForRetaliationKey) != null && GetCardPropertyJournalEntryBoolean(MarkForRetaliationKey).Value == true, RetaliationResponse, TriggerType.DealDamage, TriggerTiming.After);
         }
 
-        private IEnumerator RedirectDamageAndRetaliateResponse(DealDamageAction dd)
+        private IEnumerator RetaliationResponse(DealDamageAction dd)
         {
-            //You may redirect any damage dealt to other lion cards to this card. 
+            //If this card was dealt damage this way, this card deals the source of that damage 3 melee damage.
+            Card originalSourceOfDamage = dd.DamageSource.Card;
+            SetCardProperty(MarkForRetaliationKey, false);
+            IEnumerator coroutine = DealDamage(Card, originalSourceOfDamage, 3, DamageType.Melee, isCounterDamage: true, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+        private IEnumerator RedirectDamageResponse(DealDamageAction dd)
+        {
+            //You may redirect any damage dealt to other lion cards to this card.
+            SetCardPropertyToTrueIfRealAction(MarkForRetaliationKey);
+
             IEnumerator coroutine = GameController.RedirectDamage(dd, Card, isOptional: true, cardSource: GetCardSource());
             if (UseUnityCoroutines)
             {
@@ -32,23 +53,6 @@ namespace Studio29.TheTamer
                 GameController.ExhaustCoroutine(coroutine);
             }
 
-            //If this card was dealt damage this way...
-            if (!dd.DidDealDamage || dd.DamageSource is null || !dd.DamageSource.IsCard )
-            {
-                yield break;
-            }
-
-            //this card deals the source of that damage 3 melee damage.
-            Card originalDamageSource = dd.DamageSource.Card;
-            coroutine = DealDamage(Card, originalDamageSource, 3, DamageType.Melee, cardSource: GetCardSource());
-            if (UseUnityCoroutines)
-            {
-                yield return GameController.StartCoroutine(coroutine);
-            }
-            else
-            {
-                GameController.ExhaustCoroutine(coroutine);
-            }
         }
     }
 }
