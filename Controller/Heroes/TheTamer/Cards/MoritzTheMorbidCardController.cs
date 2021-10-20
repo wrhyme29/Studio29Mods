90a6e03df69966748634699d92b747b39fea6365
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-19 23:50:51 -0400

    Moritz the Morbid

---
+++ Controller/Heroes/TheTamer/Cards/MoritzTheMorbidCardController.cs
@@ -0,0 +1,25 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Studio29.TheTamer
+{
+    public class MoritzTheMorbidCardController : LionCardController
+    {
+
+        public MoritzTheMorbidCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, TriggerType.DestroyCard)
+        {
+
+        }
+
+        protected override IEnumerator DealtExactlyOneDamageResponse(DealDamageAction dd)
+        {
+            //destroy 1 non-hero target with 2 or fewer HP.
+            return GameController.SelectAndDestroyCard(HeroTurnTakerController, new LinqCardCriteria((Card c) => !c.IsHero && c.IsTarget && c.HitPoints <= 2, "non-hero targets with 2 or fewer HP", useCardsSuffix: false), false, cardSource: GetCardSource());
+        }
+
+
+    }
+}
