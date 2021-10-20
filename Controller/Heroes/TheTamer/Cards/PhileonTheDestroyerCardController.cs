2b8ed984c33b897d70662e4c127fa570e83cc2f5
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-20 01:04:16 -0400

    Fix missing arg

--- Controller/Heroes/TheTamer/Cards/PhileonTheDestroyerCardController.cs
+++ Controller/Heroes/TheTamer/Cards/PhileonTheDestroyerCardController.cs
@@ -15,7 +15,7 @@ namespace Studio29.TheTamer
         protected override IEnumerator DealtExactlyOneDamageResponse(DealDamageAction dd)
         {
             //it deals 2 non-hero targets 2 melee damage each.
-            return GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, Card), 2, DamageType.Melee, 2, false, , additionalCriteria: (Card c) => !c.IsHero && c.IsTarget, cardSource: GetCardSource());
+            return GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, Card), 2, DamageType.Melee, 2, false, 2, additionalCriteria: (Card c) => !c.IsHero && c.IsTarget, cardSource: GetCardSource());
         }
     }
 }
